911ea1f9b3cb7cdd1d8268a6b2b79d7ac1173b38
Tosin <tosiniyiola00@gmail.com>
2022-12-13 06:17:48 +0100

    changes

--- tiide-hr-backend/src/app.js
+++ tiide-hr-backend/src/app.js
@@ -93,7 +93,7 @@ const permission = [
   },
 ];
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
   db.permissions.bulkCreate(permission, {
     updateOnDuplicate: ['name', 'description', 'groupName', 'value'],
   });
